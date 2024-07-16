7936c0046982658c91d4f01dac1b512660040037
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-16 20:02:09 +0300

    refactoring react hook from

--- src/forms/FormField.tsx
+++ src/forms/FormField.tsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 import {
   Controller,
   ControllerProps,
+  FieldPath,
   FieldValues,
   useFormContext,
   UseFormReturn,
 } from "react-hook-form";
 
-export interface IFormFieldProps<T extends FieldValues = FieldValues>
-  extends Omit<ControllerProps<T>, "control" | "render"> {
+export interface IFormFieldProps<
+  T extends FieldValues = FieldValues,
+  TName extends FieldPath<T> = FieldPath<T>,
+> extends Omit<ControllerProps<T, TName>, "control" | "render"> {
   render: ({
     field,
     fieldState,
     formState,
-  }: Parameters<ControllerProps<T>["render"]>[0] & {
-    form: Omit<UseFormReturn<T>, "control">;
+  }: Parameters<ControllerProps<T, TName>["render"]>[0] & {
+    form: Omit<UseFormReturn<T, TName>, "control">;
   }) => React.ReactElement;
 }
 
-export const FormField = <T extends FieldValues = FieldValues>({
+export const FormField = <
+  T extends FieldValues = FieldValues,
+  TName extends FieldPath<T> = FieldPath<T>,
+>({
   render,
   ...props
-}: IFormFieldProps<T>) => {
+}: IFormFieldProps<T, TName>) => {
   const { control, ...rest } = useFormContext<T>();
 
   return (
-    <Controller<T>
+    <Controller<T, TName>
       control={control}
       {...props}
       render={field => render({ ...field, form: rest })}
     />
   );
 };
+
+export const typedFormField =
+  <T extends FieldValues = FieldValues>() =>
+  <TName extends FieldPath<T> = FieldPath<T>>(
+    props: ComponentProps<typeof FormField<T, TName>>,
+  ) =>
+    <FormField {...props} />;
