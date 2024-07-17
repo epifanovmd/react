e26c8e4a023da5539939aed51192187a82a0071e
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-17 15:13:51 +0300

    refactoring react hook from

--- src/forms/hooks/useWizardForm.ts
+++ src/forms/hooks/useWizardForm.ts
@@ -1,13 +1,7 @@
-import { useEffect, useLayoutEffect, useRef } from "react";
-import {
-  FieldValues,
-  useForm,
-  UseFormProps,
-  UseFormReturn,
-} from "react-hook-form";
+import { useRef } from "react";
+import { FieldValues, useForm, UseFormProps } from "react-hook-form";
 
-import { IWizard } from "../Wizard";
-import { IUseWizard, IUseWizardReturn } from "./useWizard";
+import { IUseWizardReturn } from "./useWizard";
 
 export const useWizardForm = <T extends FieldValues = FieldValues>(
   wizard: IUseWizardReturn<T>,
